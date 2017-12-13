85ae4ba9beb76885b9694d49b5600fbd8a55aed7
Anupam Ghosh <anupam.ghosh@siemens.com>
2017-12-13 16:31:04 +0530

    fix(lib): container.php access fix from cache

--- src/lib/php/common-container.php
+++ src/lib/php/common-container.php
@@ -50,6 +50,7 @@ else {
   if ($cacheDir && is_dir($cacheDir))
   {
     $dumper = new PhpDumper($container);
+    umask(0022);
     file_put_contents(
         $cacheFile,
         $dumper->dump(array('class' => $containerClassName))
